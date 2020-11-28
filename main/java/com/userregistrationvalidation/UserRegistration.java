b9d21fd36cbee6c082851acab4c57e7c867a9489
ManaliParab12 <manaliparab10@gmail.com>
2020-11-28 23:16:50 +0530

    Welcome to User Registration Validation Program

---
+++ main/java/com/userregistrationvalidation/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.userregistrationvalidation;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Validation");
+    }
+}
