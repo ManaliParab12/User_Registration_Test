38326ef5a0757670b85ccce911aa950b1870a844
ManaliParab12 <manaliparab10@gmail.com>
2020-11-28 23:34:34 +0530

    Added validateEmailID Method with precise @ and . Positions

--- main/java/com/userregistrationvalidation/UserRegistration.java
+++ main/java/com/userregistrationvalidation/UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
 
     String firstNamePattern = "^[A-Z]{1}[ a-z]{2,25}";
     String lastNamePattern = "^[A-Z]{1}[ a-z]{2,25}";
+    String emailPattern = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Validation");
@@ -17,5 +18,9 @@ public class UserRegistration {
 
     public boolean validateLastName(String lastName) {
         return (Pattern.matches(lastNamePattern,lastName));
+    }
+
+    public boolean validateEmailID(String emailID) {
+        return (Pattern.matches(emailPattern,emailID));
     }
 }
