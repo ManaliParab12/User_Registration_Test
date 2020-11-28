4f259cc19e32be94c7e1f4fa04174404767ecb81
ManaliParab12 <manaliparab10@gmail.com>
2020-11-28 23:25:44 +0530

    Added validFirstName Method with First letter Capital and minimum 3 Characters pattern

--- main/java/com/userregistrationvalidation/UserRegistration.java
+++ main/java/com/userregistrationvalidation/UserRegistration.java
@@ -1,8 +1,16 @@
 package com.userregistrationvalidation;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+    String firstNamePattern = "^[A-Z]{1}[ a-z]{2,25}";
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Validation");
+    }
+
+    public boolean validateFirstName(String firstName) {
+        return (Pattern.matches(firstNamePattern,firstName));
     }
 }
