95aa8579cfa06ba620114f0aac3e687be9ac2ca7
ManaliParab12 <manaliparab10@gmail.com>
2020-11-28 23:31:11 +0530

    Added validateLastName Method with First letter Capital and minimum 3 Characters pattern

--- main/java/com/userregistrationvalidation/UserRegistration.java
+++ main/java/com/userregistrationvalidation/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     String firstNamePattern = "^[A-Z]{1}[ a-z]{2,25}";
+    String lastNamePattern = "^[A-Z]{1}[ a-z]{2,25}";
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Validation");
@@ -12,5 +13,9 @@ public class UserRegistration {
 
     public boolean validateFirstName(String firstName) {
         return (Pattern.matches(firstNamePattern,firstName));
+    }
+
+    public boolean validateLastName(String lastName) {
+        return (Pattern.matches(lastNamePattern,lastName));
     }
 }
