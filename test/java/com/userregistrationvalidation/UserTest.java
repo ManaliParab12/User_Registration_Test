a925ef0a308559c114414f8140c53502e2123803
ManaliParab12 <manaliparab10@gmail.com>
2020-11-29 11:12:28 +0530

    Created Valid Test case to check First name

--- test/java/com/userregistrationvalidation/UserTest.java
+++ test/java/com/userregistrationvalidation/UserTest.java
@@ -1,4 +1,17 @@
 package com.userregistrationvalidation;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserTest {
+
+    UserRegistration userregistration = new UserRegistration();
+
+    //TEST TO CHECK  FIRST NAME VALID
+    @Test
+    public void givenFirstName_whenFirstLetterCapital_thenReturnTrue() {
+        String name = "Manali";
+        boolean firstName = userregistration.validateFirstName(name);
+        Assert.assertTrue(firstName);
+    }
 }
