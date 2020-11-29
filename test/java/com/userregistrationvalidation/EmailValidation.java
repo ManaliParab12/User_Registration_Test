1fa8b07cf7792d7609d11901b3ee3062a7edde1f
ManaliParab12 <manaliparab10@gmail.com>
2020-11-29 20:58:01 +0530

    Defined Lambda Function to validate user Input

--- test/java/com/userregistrationvalidation/EmailValidation.java
+++ test/java/com/userregistrationvalidation/EmailValidation.java
@@ -11,7 +11,7 @@ import org.junit.runners.Parameterized;
 public class EmailValidation {
      String emailAddress;
      boolean expected;
-     
+
     public EmailValidation(String emailAddress, boolean expected) {
         this.emailAddress = emailAddress;
         this.expected = expected;
